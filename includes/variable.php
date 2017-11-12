7b072d3c94fb423c9e1a8f0df3077ca9a31e4c5a
Bols <lenovobolante@gmail.com>
2017-11-12 23:55:37 +0800

    ggs
    gsgs

--- includes/variable.php
+++ includes/variable.php
@@ -1,10 +1,10 @@
 <?php
-$title="Pilar - Evaluation System";
-$keywords = "Pilar - Evaluation System";
+$title="HRIS DTR - Human Resources Information System Daily Time Record";
+$keywords = "HRIS DTR - Human Resources Information System Daily Time Record";
 $hostname = 'localhost';
 $username = 'root';
 $password = '1234';
 $database = 'hrisdtr';
-$pathdomain = 'http://localhost/HRISZCIPSI/HRISZCIPSI/';
+$pathdomain = 'http://localhost/HRIS-DTR/';
 error_reporting(E_ERROR | E_PARSE);
 ?>
