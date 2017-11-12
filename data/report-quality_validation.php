70352b08976d168d4fba8f821678c6604aaf9c76
Bols <lenovobolante@gmail.com>
2017-11-12 23:55:30 +0800

    fas
    fafaf

--- data/report-quality_validation.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include_once('class/clsConnection.php');
-include('includes/variable.php');
-include('session.php');
-include('sessionuser.php');
-	$con = new mycon();
-	$con->getconnect();
-
-error_reporting(E_ERROR | E_PARSE);?>
